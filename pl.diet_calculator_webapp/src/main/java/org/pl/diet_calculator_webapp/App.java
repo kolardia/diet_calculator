12be5b93e08cca2e6c8ab2a4bf46e84d1928495b
Monika <monika@debian.test.local>
2019-06-07 15:55:51 +0200

    modules: webapp, application, domain

---
+++ pl.diet_calculator_webapp/src/main/java/org/pl/diet_calculator_webapp/App.java
@@ -0,0 +1,13 @@
+package org.pl.diet_calculator_webapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
